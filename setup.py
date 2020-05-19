b5ca46715f42dde1a086ce9cad261cd20687d28a
Gratiela Ghergu <alina.ghergu@gmail.com>
2020-05-19 12:26:14 +0200

    Pin dependencies in setup.py

--- setup.py
+++ setup.py
@@ -10,8 +10,11 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-with open('requirements.txt') as requirements_file:
-    requirements = requirements_file.read()
+requirements = [
+        'requests>=2.23',
+        'psycopg2>=2.8',
+        'kafka-python>=2.0'
+    ]
 
 setup_requirements = [ ]
 
